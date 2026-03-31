b91bf921ea5cd7458c7ca87f52fb4a04063195d9
MHAMMAD101 <mhammadadebat200@gmail.com>
2026-03-31 10:27:53 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Serve static files from frontend folder
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 // Test routes
 app.get('/', (req, res) => {
